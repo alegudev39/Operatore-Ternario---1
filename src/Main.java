05be134727f7c0787f65318ef0b4575a71c51780
AleDev <aledev@example.com>
2024-03-19 11:31:14 +0100

    consegna

---
+++ src/Main.java
@@ -0,0 +1,32 @@
+//Copia il seguente programma Java e sostutuisci il costrutto if-else con l'operatore ternario
+//
+//public class Main {
+//    public static void main(String[] args) {
+//        for (int i = 1; i <= 100; i++) {
+//            System.out.println("Valore: " + i);
+//            System.out.println(checkValue(i));
+//        }
+//    }
+//
+//    private static String checkValue(int value) {
+//        String result = "";
+//
+//        if (value % 5 == 0) {
+//            result = "FizzBuzz";
+//        } else  {
+//            result = "Buzz";
+//        }
+//        return result;
+//    }
+//
+//}
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(checkValue(10));
+    }
+
+    private static String checkValue(int value) {
+        String result = (value % 5 == 0) ? (result = "FizzBuzz") : (result = "Buzz");
+        return result;
+    }
+}
